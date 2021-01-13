d43ee61268186122b0ab1beb5a17f1cd96d36293
Ahmad Fahmi Aisar <fahmiaisar99@gmail.com>
2021-01-14 02:23:33 +0700

    ui perwalian

---
+++ app/src/main/java/inn/mroyek/bismillahsiakad/presentation/ui/perwalian/PerwalianActivity.kt
@@ -0,0 +1,15 @@
+package inn.mroyek.bismillahsiakad.presentation.ui.perwalian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import inn.mroyek.bismillahsiakad.R
+
+class PerwalianActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perwalian)
+    }
+
+    fun goBack(view: View) {}
+}
